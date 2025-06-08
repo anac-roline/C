f637a63d91b640630f20b8f1fb5022a107432e80
Ana Caroline <lealanacaroline00@gmail.com>
2025-06-08 14:46:46 -0300

    Add files via upload

---
+++ laco_for/exemplo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    for (int i = 1; i <= 5; i++){ // i é a variável de controle, iniciada em 1
+        //Condição: enquanto i for menor ou igual a 5
+        //Incremento: i aumenta 1 a cada iteração
+        printf("%d\n", i);
+    }
+    return 0;
+}
